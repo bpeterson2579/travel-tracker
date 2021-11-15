68b89c9ecc8571c2cad15d3e42c728cfef300837
Brian Peterson <bpeterson1579@gmail.com>
2021-11-15 16:21:58 -0700

    Add minor formatting to classes

--- src/Destinations.js
+++ src/Destinations.js
@@ -1,7 +1,13 @@
 class Destinations {
   constructor(data) {
     this.data = data;
-  }
-}
+  };
+
+  retrieveTravelerDestinations(id) {
+    return this.data.filter(destination => {
+      return destination.id === id;
+    });
+  };
+};
 
 module.exports = Destinations;
