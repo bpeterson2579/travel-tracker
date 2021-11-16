9b8bc3c69f066e7973b9f6287b34223d79838054
Brian Peterson <bpeterson1579@gmail.com>
2021-11-16 14:05:01 -0700

    Add tests for Destination class

--- test/Destinations-test.js
+++ test/Destinations-test.js
@@ -1,12 +1,19 @@
 const expect = require('chai').expect;
 const Destinations = require('../src/Destinations.js');
+const Traveler = require('../src/Traveler.js');
 import {tripsData} from './test-data.js';
 
 describe('Destinations', () => {
   let destinations;
+  let traveler;
 
   beforeEach(function() {
     destinations = new Destinations(tripsData);
+    traveler = new Traveler({
+      id: 1,
+      name: "Ham Leadbeater",
+      travelerType: "relaxer"
+    });
   });
 
   it('should be a function', function() {
@@ -21,4 +28,17 @@ describe('Destinations', () => {
     expect(destinations.data.length).to.equal(10);
   });
 
-})
+  it('should match all destinations to the proper traveler by id', function() {
+
+    expect(destinations.retrieveTravelerDestinations(1)).to.deep.equal([{
+      id: 1,
+      userID: 1,
+      destinationID: 1,
+      travelers: 1,
+      date: '2022/09/16',
+      duration: 8,
+      status: 'approved',
+      suggestedActivities: []
+    }]);
+  });
+});
