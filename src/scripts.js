dcaff8dea10972dc64bfa748e810ae638d39d972
Brian Peterson <bpeterson1579@gmail.com>
2021-11-16 20:47:43 -0700

    Add login screen

--- src/scripts.js
+++ src/scripts.js
@@ -15,22 +15,32 @@ const submitTripButton = document.getElementById('submitTripButton');
 const startDate = document.getElementById('startDate');
 const duration = document.getElementById('duration');
 const numPassengers = document.getElementById('numPassengers');
-// const tripList = document.getElementById('tripList');
+const mainScreen = document.querySelector('main');
+const submitUserPass = document.getElementById('submitUserPass');
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const err = document.getElementById('errorPassword');
+const loginBox = document.getElementById('loginBox');
 
+let travelers;
 let traveler;
 let trips;
 let destinations;
 
 const grabData = () => {
   Promise.all([fetchData('travelers'), fetchData('trips'), fetchData('destinations')])
-    .then(data => createTravelerDashboard(data))
+    .then(data => createDataSet(data))
 };
 
-const createTravelerDashboard = (data) => {
-  let travelers = new TravelersRepo(data[0].travelers);
-  traveler = new Traveler(travelers.data[travelers.returnRandomTraveler()]);
+const createDataSet = (data) => {
+  travelers = new TravelersRepo(data[0].travelers);
+  // traveler = new Traveler(travelers.data[travelers.returnRandomTraveler()]);
   trips = new Trips(data[1].trips);
   destinations = new Destinations(data[2].destinations);
+}
+
+const createTraveler = (id) => {
+  traveler = new Traveler(travelers.data[id - 1]);
   traveler.findTrips(trips.data);
   traveler.findDestinations(destinations.data);
   traveler.findPastTrips();
@@ -64,6 +74,19 @@ const updatePending = (dataObj) => {
   traveler.pendingTrips.push(dataObj)
 }
 
+const login = () => {
+  if(password.value === 'travel') {
+    domUpdates.show(mainScreen);
+    domUpdates.hide(loginBox);
+    const num = username.value.split('')
+    const id = Number(`${num[8]}${num[9]}`);
+    createTraveler(id)
+  }else {
+    err.innerText = 'Password is incorrect, try again.'
+  }
+}
+
+submitUserPass.addEventListener('click', login);
 submitTripButton.addEventListener('click', createTripRequest);
 newTripButton.addEventListener('click', domUpdates.showForm);
 window.addEventListener('load', grabData);
